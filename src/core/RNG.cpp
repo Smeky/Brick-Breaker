ad62bc8cdc8fde6fc876e72936a05505fda93610
Smeky <smeky.sobota@gmail.com>
2016-01-02 23:49:36 +0100

    RNG impl

--- src/core/RNG.cpp
+++ src/core/RNG.cpp
@@ -1,8 +1,35 @@
 #include "RNG.hpp"
 
+#include <chrono>
+
 namespace bb {
+
+// Initialize static members
+// Setup seed and Pseudo-Random Generator
+uint32_t RNG::m_seed = std::chrono::high_resolution_clock::now().time_since_epoch().count();
+std::mt19937 RNG::m_generator( RNG::m_seed );
 
 RNG::RNG()
 {}
+
+void RNG::setSeed( uint32_t seed ) {
+    m_seed = seed;
+
+    m_generator.seed( m_seed );
+}
+
+uint32_t RNG::getSeed() {
+    return m_seed;
+}
+
+float RNG::randomNormal() {
+    std::uniform_real_distribution<float> dist( 0, 1 );
+    return dist( m_generator );
+}
+
+double RNG::randomNormalDouble() {
+    std::uniform_real_distribution<double> dist( 0, 1 );
+    return dist( m_generator );
+}
 
 } // namespace bb
--- src/core/RNG.hpp
+++ src/core/RNG.hpp
@@ -1,14 +1,45 @@
 #pragma once
 
+#include <random>
+
 namespace bb {
 
 class RNG {
 public:
     RNG();
 
+    static void setSeed( uint32_t seed );
+    static uint32_t getSeed();
+
+    template <class IntType = int>
+    static IntType randomInt( IntType from = 0, IntType to = std::numeric_limits<IntType>::max() );
+
+    template <class RealType = float>
+    static RealType randomReal( RealType from = 0, RealType to = std::numeric_limits<RealType>::max() );
+
+    static float randomNormal();
+    static double randomNormalDouble();
+
 private:
-
-
+    static uint32_t m_seed;
+    static std::mt19937 m_generator;
 };
+
+template <class IntType>
+IntType RNG::randomInt( IntType from /* = 0 */,
+                        IntType to /* = std::numeric_limits<IntType>::max() */ )
+{
+    std::uniform_int_distribution<IntType> dist( from, to );
+    return dist( m_generator );
+}
+
+template <class RealType /* = float*/>
+RealType RNG::randomReal( RealType from /* = 0 */,
+                          RealType to /* = std::numeric_limits<RealType>::max() */ )
+{
+    std::uniform_real_distribution<RealType> dist( from, to );
+    return dist( m_generator );
+}
+
 
 } // namespace bb
